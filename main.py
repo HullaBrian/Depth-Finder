efdd69cc9dccb51c9782b71135ac7bca1c8e6a62
Lionskey <Logan@Lionskey.com>
2021-10-14 14:56:31 -0500

    Update codeset for superior prompt based CLA

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 import packageManager  # makes sure all necessary packages are installed
-import click  # imports click libarary
+
+import art
+
+art.tprint("Phishing-Detective")
 
 
-@click.group()
-@click.option('--set', default="")
-def set(command):
-    print("Set received command" + command)
-    pass
+def main():
 
+    prompt = "pd> "
+    i = input(prompt)
 
-@set.group()
-@click.option("--hostname", default="", help="sets the hostname")
-@click.argument("hostname", default="")
-def sethostname(hostname):
-    click.echo("Set host name to " + hostname)
+    if i == "help" or i == "?":
+        print(" ")
+        print("Commands")
+        print("=============")
+        print("\"help\" or \"?\" ---- prints this help menu")
+        print(" ")
+        main()
 
+    if i == "exit":
+        exit()
 
-if __name__ == '__main__':
-    set()
-
-
-"""
-Current Error:
-    Input: >python main.py --set --hostname https://google.com/
-    Error message:
-    
-        Verifying required libraries...Done!
-        Usage: main.py [OPTIONS] COMMAND [ARGS]...
-        Try 'main.py --help' for help.
-        
-        Error: No such command 'https://google.com/'.
-        
-"""
-
+main()
