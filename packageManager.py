345e1f397503523c69053c19827106c84f8304a8
HullaBrian <62084657+HullaBrian@users.noreply.github.com>
2021-10-14 10:32:33 -0500

    Helpful messages when verifying required library "click" is installed one the user's device.

--- packageManager.py
+++ packageManager.py
@@ -1,5 +1,10 @@
 import os
 try:
+    print("Verifying required libraries...", end="")
     import click  # Tries to import
+    print("Done!")
 except ModuleNotFoundError:
+    click.echo("Could not find required library.")
+    click.echo("Installing required libraries...", nl=False)
     os.system("pip install -U click")  # Installs click if not already installed
+    click.echo("Done!")
