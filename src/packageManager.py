40101850f40160dd9ae5844692949466c06b778b
HullaBrian <62084657+HullaBrian@users.noreply.github.com>
2021-11-25 17:19:13 -0600

    Removed the weird selenium crap going on in main.py and packageManager.py. Also added some more functionality to installTor.py

---
+++ src/packageManager.py
@@ -0,0 +1,34 @@
+import os
+
+
+missingLibrary = False
+print("Verifying required libraries...")
+try:
+    import art  # Tries to import
+except ModuleNotFoundError:
+    missingLibrary = True
+    print("Could not find a required library. Installing...", end="")
+    os.system("pip install -U art -q")
+    print("Done!")
+try:
+    import whois
+except ModuleNotFoundError:
+    missingLibrary = True
+    print("Could not find a required library. Installing...", end="")
+    os.system("pip install -U python-whois -q")
+    print("Done!")
+try:
+    import requests
+    os.system("pip install -U requests[socks] -q")
+except ModuleNotFoundError:
+    missingLibrary = True
+    print("Could not find a required library. Installing...", end="")
+    os.system("pip install -U requests - q")
+    print("Done!")
+
+# https://pypi.org/project/webscreenshot/
+
+if missingLibrary:
+    print("Restarting to refresh content...")
+    import main
+    exit()
