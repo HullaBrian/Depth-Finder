6e951d100ffba046318ab70c9e945afa24374f4a
Logan Goins <55106700+Lionskey@users.noreply.github.com>
2022-07-21 18:21:43 -0400

    Update installTor.py
    Added more distribution support than just Debian based systems

--- installTor.py
+++ installTor.py
@@ -3,14 +3,32 @@ import ssl
 import subprocess
 import time
 import sys
+import distro
 
 
 def installTor():
     if sys.platform.startswith('linux'):
-        print("Installing Tor...", end="")
-        os.system("sudo apt install tor")
-        print("Done!")
-        return
+        try:
+            print("Installing Tor...", end="")
+            linux_type = distro.id()
+            if str(linux_type) == "gentoo":
+                os.system("sudo emerge --ask net-vpn/tor")
+            if str(linux_type) == "debian" or str(linux_type) == "ubuntu" or str(linux_type) == "linuxmint" or str(linux_type) == "raspbian":
+                os.system("sudo apt install tor")
+            if str(linux_type) == "arch":
+                os.system("sudo pacman -S tor")
+            if str(linux_type) == "rhel" or str(linux_type) == "centos":
+                print("You are going to have to enable EPEL repo and add tor yourself. Use this link for help https://community.torproject.org/relay/setup/bridge/centos-rhel/")
+            if str(linux_type) == "fedora":
+                os.sytsem("sudo dnf install tor")
+            if str(linux_type) == "opensuse":
+                os.system("sudo zypper install tor")
+
+
+            print("Done!")
+            return
+        except:
+            print("sorry the automated Tor install process for your distribution has failed. You will have to install Tor yourself")
 
     elif sys.platform.startswith('win32'):
         windowsURL = "https://www.torproject.org/dist/torbrowser/11.0.1/torbrowser-install-win64-11.0.1_en-US.exe"
